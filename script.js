409a9a7a3f8416913f766313a854e6b08a4d6393
dubemmbah <dubemmbah@gmail.com>
2024-03-07 14:32:43 +0100

    feat: update

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 "use strict";
 
+function validateEmailInput(email) {
+  if (typeof email !== "string" || email.indexOf("@") === -1) return false;
+
+  const [localPart, domain] = email.split("@");
+
+  if (localPart.length === 0 || domain.length < 3) return false;
+
+  const domainExtension = domain.split(".");
+
+  if (
+    domainExtension.length < 2 ||
+    domainExtension[domainExtension.length - 1].length < 2
+  )
+    return false;
+
+  return true;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const firstNameInput = document.getElementById("first-name");
   const lastNameInput = document.getElementById("last-name");
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
   const btnSubmit = document.querySelector(".btn-submit");
+  const message = document.querySelector(".message");
+
+  const inputElements = [
+    firstNameInput,
+    lastNameInput,
+    emailInput,
+    passwordInput,
+  ];
 
   console.log(firstNameInput);
   console.log(btnSubmit);
-  // const formValidation = function () {};
 
   btnSubmit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -18,6 +43,28 @@ document.addEventListener("DOMContentLoaded", function () {
     displayErrorMessage(lastNameInput);
     displayErrorMessage(emailInput);
     displayErrorMessage(passwordInput);
+
+    if (!allInputsValid(inputElements)) {
+      console.log("All inputs valid");
+
+      // Clear input values
+      firstNameInput.value =
+        lastNameInput.value =
+        emailInput.value =
+        passwordInput.value =
+          "";
+
+      // Blur fields
+      firstNameInput.blur();
+      lastNameInput.blur();
+      emailInput.blur();
+      passwordInput.blur();
+
+      message.style.animation = "messageAnimation 5s ease-in 0.75s";
+    } else {
+      console.log("Some invalid inputs");
+    }
+    // console.log(allInputsValid(inputElements));
   });
 
   function displayErrorMessage(inputElement) {
@@ -25,10 +72,47 @@ document.addEventListener("DOMContentLoaded", function () {
       .closest(".input-wrapper")
       .querySelector(".error-msg");
 
+    const errorInput = inputElement;
+
+    // if (errorInput === emailInput) {
+    //   errorInput.setAttribute("placeholder", "email@example.com");
+    //   if (!validateEmailInput(errorInput.value)) {
+    //     errorMessage.style.display = "flex";
+    //     errorInput.classList.add("error");
+    //     return;
+    //   }
+    // }
+
+    if (errorInput === emailInput) {
+      if (!validateEmailInput(errorInput.value)) {
+        errorMessage.style.display = "flex";
+        errorInput.classList.add("error");
+        errorInput.classList.add("error-input");
+        errorInput.placeholder = "email@example.com";
+        return;
+      } else {
+        errorInput.classList.remove("error-input");
+        errorInput.placeholder = "Email address";
+      }
+    }
+
     if (inputElement.value === "") {
       errorMessage.style.display = "flex";
+      errorInput.classList.add("error");
     } else {
       errorMessage.style.display = "none";
+      errorInput.classList.remove("error");
     }
+  }
+
+  function allInputsValid(elements) {
+    // if (elements.forEach((el) => el.classList.contains("error"))) {
+    //   // return true;
+    //   console.log("I have the error class");
+    // } else {
+    //   // return false;
+    //   console.log("Unavailable");
+    // }
+    return elements.some((el) => el.classList.contains("error"));
   }
 });
